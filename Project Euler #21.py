c414c6df87ec4114224c5e3c85c7aff1dcb9840b
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-15 08:32:52 +0200

    Update Project Euler #21.py

--- Project Euler #21.py
+++ Project Euler #21.py
@@ -2,17 +2,21 @@ import time
 import math
 start = time.time()
 divisor_sums = []
+amicable = []
+
 def divisors(n):
     divisor_list = []
-    for x in range(1,int(math.sqrt(n)+1)):
+    for x in range(1, int(math.sqrt(n) + 1)):
         if n % x == 0:
-            divisor_list.append(int(n/x))
+            divisor_list.append(int(n / x))
             if x < math.sqrt(n):
                 divisor_list.append(x)
-    divisor_sums.append(sum(divisor_list)-n)
-                
-for x in range(1,100):
+    divisor_sums.append(sum(divisor_list) - n)
+
+for x in range(1,10000):
     divisors(x)
+
+print(divisor_sums)
 
 
 def Amicable_numbers(upper_bound):
