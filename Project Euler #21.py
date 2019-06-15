a3caadf14254d5d70022096b7513a46456eb22d5
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-15 08:47:49 +0200

    Update Project Euler #21.py

--- Project Euler #21.py
+++ Project Euler #21.py
@@ -16,28 +16,8 @@ def divisors(n):
     divisor_number.append(n)
     
     
-for x in range(220,230):
+for x in range(1,10000):
     divisors(x)
 
-
-
-def Amicable_numbers(upper_bound):
-    for x in range(1, upper_bound):
-        for y in range(1,x):
-            if x % y == 0:
-                divisors_a.append(y)
-        for a in range(1, sum(divisors_a)-1):
-            if sum(divisors_a) % a == 0:
-                divisors_b.append(a)
-        if x == sum(divisors_b):
-            numbers.append(x)
-            divisors_a.clear()
-            divisors_b.clear()
-        else:
-            divisors_a.clear()
-            divisors_b.clear()
-
-Amicable_numbers(10000)
-print(sum(numbers))
 end = time.time()
 print(end - start)
