e858a99d9f10c339f4ba1633b2acc5f0be3e16e8
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-15 10:23:14 +0200

    Update Project Euler #21.py

--- Project Euler #21.py
+++ Project Euler #21.py
@@ -1,23 +1,39 @@
 import time
 import math
+
 divisor_sums = []
-divisor_number = []
-amicable = []
 start = time.time()
+
 
 def divisors(n):
     divisor_list = []
-    for x in range(1, int(math.sqrt(n) + 1)):
+    for x in range(1, int(math.sqrt(n)+1)):
         if n % x == 0:
-            divisor_list.append(int(n / x))
+            divisor_list.append(int(n/x))
             if x < math.sqrt(n):
                 divisor_list.append(x)
-    divisor_sums.append(sum(divisor_list) - n)
-    divisor_number.append(n)
-    
-    
-for x in range(1,10000):
+    if (sum(divisor_list)-n) < 10001:
+        divisor_sums.append(sum(divisor_list)-n)
+    else:
+        divisor_sums.append(0)
+
+
+for x in range(1,10001):
     divisors(x)
+
+
+print(divisor_sums[219])
+print(divisor_sums[283])
+for x in range(1,10000):
+    y = divisor_sums[x-1]
+    z = divisor_sums[y-1]
+    if y == z:
+        print(x)
+
+
+
+
+
 
 end = time.time()
 print(end - start)
